e7e0dd4bfa9e59e0a04e84ed98a2ac62bf3192e7
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-12 12:46:19 +0530

    Add files via upload

---
+++ springboot-dynamodb/src/main/java/com/example/springbootdynamodb/config/DynamoDbConfig.java
@@ -0,0 +1,39 @@
+package com.example.springbootdynamodb.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDbConfig {
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "dynamodb.us-east-1.amazonaws.com",
+                                "us-east-1"
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        "AKIAYC32WENM4TMICD5B",
+                                        "RO+rGSxb8IzoGdyatGTviEoU1C8a6nBTCjBXA9Ri"
+                                )
+                        )
+                )
+                .build();
+    }
+}
